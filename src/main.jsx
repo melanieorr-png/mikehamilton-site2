7b851dd348763ba21c2145783bb3e79d73d0fb7b
melanieorr-png <melanieorr@gmail.com>
2026-04-20 18:17:55 +0930

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MikeHamiltonVETConsulting from "./App.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MikeHamiltonVETConsulting />
+    <App />
   </React.StrictMode>
 );
